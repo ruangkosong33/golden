09b239866d241a5f296f4924ded9e60fa637f08a
ruangkosong33 <ruangkosong33@ruangkosong33@gmail.com>
2022-11-20 19:36:57 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 Route::get('/category', [CategoryController::class,'index'])->name('category.index');
 Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
 Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
-Route::put('/category/{id}', [CategoryController::class, 'update'])->name('category.update');
 Route::get('/category{id}', [CategoryController::class, 'edit'])->name('category.edit');
+Route::put('/category/{id}', [CategoryController::class, 'update'])->name('category.update');
 Route::delete ('/category/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
 
