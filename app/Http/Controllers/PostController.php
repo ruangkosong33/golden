f76369e0cce78ea48847cc015ecf8d7cfcce6820
ruangkosong33 <ruangkosong33@ruangkosong33@gmail.com>
2022-11-20 21:14:32 +0800

    change

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,19 +11,19 @@ class PostController extends Controller
 {
     public function index()
     {
-        $post=DB::table('post')->paginate('7');
+        $post=DB::table('posts')->paginate('7');
 
-        return view('admin.pages.post.index_post', ['post'=>$post]);
+        return view('admin.pages.posts.index_posts', ['post'=>$post]);
     }
 
     public function create()
     {
-        return view('admin.pages.post.create_post');
+        return view('admin.pages.posts.create_posts');
     }
 
     public function store(Request $request)
     {
-        $this->validate([$request,
+        $this->validate($request,[
             'title_post'=>'required',
         ]);
 
@@ -32,7 +32,6 @@ class PostController extends Controller
             'slug'=>Str::slug($request->title_category),
             'body'=>$request->body,
             'is_active'=>$request->is_active,
-            'views'=>$request->views,
         ]);
 
         return redirect()->route('posts.index')->with('message', 'Data Post Berhasl Di Tambahkan');
@@ -40,20 +39,22 @@ class PostController extends Controller
 
     public function edit($id)
     {
-        $post=DB::table('post')->where('id', $id)->first();
+        $post=DB::table('posts')->where('id', $id)->first();
 
-        return view('admin.pages.edit_post', ['post'=>$post]);
+        return view('admin.pages.edit_posts', ['post'=>$post]);
     }
 
     public function update(Request $request, $id)
     {
-        $this->validate('post')->update($request,[
+        $this->validate($request,[
             'title_post'=>'required',
         ]);
 
-        DB::table('post')->update([
+        DB::table('post')->where('id', $request->id)->update([
             'title_post'=>$request->title_post,
             'slug'=>Str::slug($request->title_post),
+            'body'=>$request->body,
+            'is_active'=>$request->is_active,
         ]);
     }
 }
