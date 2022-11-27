7b4cf7c5ed83ccd7c489de4f46c3cc441859138c
ruangkosong33 <ruangkosong33@ruangkosong33@gmail.com>
2022-11-27 13:11:18 +0800

    model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,69 +16,71 @@ class PostController extends Controller
         //                     ->join('categorys', 'posts.categorys_id', '=','categorys.id')
         //                     ->select('posts.*', 'categorys.title_category')
         //                     ->paginate('5');
+
+        $post=Post::orderBy('id')->latest()->paginate('5');
     
         return view('admin.pages.posts.index_posts', ['post'=>$post]);
     }
 
-    public function create()
-    {
-        return view('admin.pages.posts.create_posts');
-    }
+//     public function create()
+//     {
+//         return view('admin.pages.posts.create_posts');
+//     }
 
-    public function store(Request $request)
-    {
-        $this->validate($request,[
-            'title_post'=>'required',
-        ]);
+//     public function store(Request $request)
+//     {
+//         $this->validate($request,[
+//             'title_post'=>'required',
+//         ]);
 
-        DB::table('post')->insert([
-            'title_post'=>$request->title_post,
-            'slug'=>Str::slug($request->title_post),
-            'body'=>$request->body,
-            'picture_post'=>$request->picture_post,
-            'is_active'=>$request->is_active,
-        ]);
+//         DB::table('post')->insert([
+//             'title_post'=>$request->title_post,
+//             'slug'=>Str::slug($request->title_post),
+//             'body'=>$request->body,
+//             'picture_post'=>$request->picture_post,
+//             'is_active'=>$request->is_active,
+//         ]);
 
-        return redirect()->route('posts.index')->with('message', 'Data Post Berhasl Di Tambahkan');
-    }
+//         return redirect()->route('posts.index')->with('message', 'Data Post Berhasl Di Tambahkan');
+//     }
 
-    public function edit($id)
-    {
-        $post=DB::table('posts')->where('id', $id)->first();
+//     public function edit($id)
+//     {
+//         $post=DB::table('posts')->where('id', $id)->first();
 
-        return view('admin.pages.edit_posts', ['post'=>$post]);
-    }
+//         return view('admin.pages.edit_posts', ['post'=>$post]);
+//     }
 
-    public function update(Request $request, $id)
-    {
-        if(empty($request->file('picture_post')))
-        {
-           $picture_post=$request->file('picture_post');
-           $picture_post1=$request->$picture_post->getClientOriginalName();
-           $picture_post2=$request->picture_post->getClientOriginalExtenstion();
-           $path=$request->file('picture_post')->storeAs('public/post-image', $picture_post2);     
-        }else {
-            $picture_post='no_image';
-        }
-        $this->validate($request,[
-            'title_post'=>'required',
-        ]);
+//     public function update(Request $request, $id)
+//     {
+//         if(empty($request->file('picture_post')))
+//         {
+//            $picture_post=$request->file('picture_post');
+//            $picture_post1=$request->$picture_post->getClientOriginalName();
+//            $picture_post2=$request->picture_post->getClientOriginalExtenstion();
+//            $path=$request->file('picture_post')->storeAs('public/post-image', $picture_post2);     
+//         }else {
+//             $picture_post='no_image';
+//         }
+//         $this->validate($request,[
+//             'title_post'=>'required',
+//         ]);
 
-        DB::table('post')->where('id', $request->id)->update([
-            'title_post'=>$request->title_post,
-            'slug'=>Str::slug($request->title_post),
-            'body'=>$request->body,
-            'picture_post'=>$request->picture_post,
-            'is_active'=>$request->is_active,
-        ]);
+//         DB::table('post')->where('id', $request->id)->update([
+//             'title_post'=>$request->title_post,
+//             'slug'=>Str::slug($request->title_post),
+//             'body'=>$request->body,
+//             'picture_post'=>$request->picture_post,
+//             'is_active'=>$request->is_active,
+//         ]);
 
-        return redirect()->route('posts.index')->with('message', 'Data Post Berhasil Di Update');
-    }
+//         return redirect()->route('posts.index')->with('message', 'Data Post Berhasil Di Update');
+//     }
 
-    public function destroy($id)
-    {
-        $post=DB::table('posts')->where('id', $id)->delete();
+//     public function destroy($id)
+//     {
+//         $post=DB::table('posts')->where('id', $id)->delete();
 
-        return redirect('category.index')->with('message', 'Data Post Berhasil Di Hapus');
-    }
+//         return redirect('category.index')->with('message', 'Data Post Berhasil Di Hapus');
+//     }
 }
---
+++ app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $table='posts';
+
+    protected $fillable=['title_post', 'slug', 'body', 'categorys_id', 'picture_post', 'is_active', 'views'];
+
+    protected $hidden=[];
+
+    
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'categorys_id', 'id');
+    }
+}
